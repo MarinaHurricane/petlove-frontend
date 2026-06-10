999bd7924413c654ae78341530fa85b7bdb69139
MarinaHurricane <maryishch88@gmail.com>
2026-06-11 00:23:36 +0100

    select by gender and select by type

--- src/pages/PetsPage/PetsPage.tsx
+++ src/pages/PetsPage/PetsPage.tsx
@@ -1,56 +1,95 @@
-import css from './PetsPage.module.css'
-import { Title } from "../../components/Title/Title"
-import { SearchBar } from '../../components/SearchBar/SearchBar'
-import { useState } from 'react'
-import Select from 'react-select'
-import axios from 'axios'
-import { useQuery } from '@tanstack/react-query'
+import css from "./PetsPage.module.css";
+import { Title } from "../../components/Title/Title";
+import { SearchBar } from "../../components/SearchBar/SearchBar";
+import { useState } from "react";
+import Select from "react-select";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
 
 export const PetsPage = () => {
-    const [page, setPage] = useState(1);
-    const [query, setQuery] = useState('');
-    const [category, setCategory] = useState('');
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState("");
+  const [category, setCategory] = useState("");
 
-      const handleSearch = (newQuery) => {
+  const handleSearch = (newQuery) => {
     setQuery(newQuery);
     setPage(1);
     // const options =  await getCategories();
   };
-      const getCategories = async() => {
-        const { data } = await axios.get('https://petlove-backend-jniu.onrender.com/api/pets/categories');
-        console.log(data);
-        return data;
+  const getCategories = async () => {
+    const { data } = await axios.get(
+      "https://petlove-backend-jniu.onrender.com/api/pets/categories",
+    );
+    console.log(data);
+    return data;
+  };
+
+    const getSpecies = async () => {
+    const { data } = await axios.get(
+      "https://petlove-backend-jniu.onrender.com/api/pets/species",
+    );
+    console.log(data);
+    return data;
+  };
+
+      const getGender = async () => {
+    const { data } = await axios.get(
+      "https://petlove-backend-jniu.onrender.com/api/pets/gender",
+    );
+    console.log(data);
+    return data;
+  };
+
+  const { data: categories } = useQuery({
+    queryKey: ["categories"],
+    queryFn: getCategories,
+  });
+  console.log(categories);
+
+    const { data: species } = useQuery({
+    queryKey: ["species"],
+    queryFn: getSpecies,
+  });
+
+     const { data: gender } = useQuery({
+    queryKey: ["gender"],
+    queryFn: getGender,
+  });
+
+  const speciesOptions = species?.map(item => {
+    return {
+      value: item,
+      label: item[0].toUpperCase() + item.slice(1),
     }
+  })
 
-    const {data: categories} = useQuery({
-        queryKey: ['categories'],
-        queryFn: getCategories,
-    })
-    console.log(categories);
+  const categoryOptions = categories?.map((category) => {
+    return {
+      value: category,
+      label: category[0].toUpperCase() + category.slice(1),
+    };
+  });
 
-    const options = categories.map(category => {
-        return {value: category, label: category[0].toUpperCase() + category.slice(1)}
-    })
-    return (
-        <>
-        <Title>Find your favorite pet</Title>
-        <div className={css.filtersContainer}>
-            <SearchBar onSearch={handleSearch}/>
-            <div className={css.categoryGender}>
-               <Select
-            //    options={[{value: 'show all', label: 'Show all'},
-            //     {value: 'sell', label: 'Sell'},
-            //      {value: 'free', label: 'Free'},
-            //      {value: 'lost', label: 'Lost'},
-            //      {value: 'found', label: 'Found'},]}/>
-           
-            options={options}
-                 placeholder='Category'/>
-       
+    const genderOptions = gender?.map((item) => {
+    return {
+      value: item,
+      label: item[0].toUpperCase() + category.slice(1),
+    };
+  });
 
-            </div>
 
+  return (
+    <>
+      <Title>Find your favorite pet</Title>
+      <div className={css.filtersContainer}>
+        <SearchBar onSearch={handleSearch} />
+        <div className={css.categoryGender}>
+          <Select className={css.select} options={categoryOptions} placeholder="Category" />
+            <Select className={css.select} options={genderOptions} placeholder="By gender" />
+            
         </div>
-        </>
-    )
-}
+             <Select options={speciesOptions} placeholder="By type" />
+      </div>
+    </>
+  );
+};
