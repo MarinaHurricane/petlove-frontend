f16fbcde788a090b2f49c4ea92cd2a1681fcc1a5
MarinaHurricane <maryishch88@gmail.com>
2026-07-21 02:46:49 +0100

    login form inputs validation styles and logic

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,5 +1,5 @@
 import css from "./LoginForm.module.css";
-import { useForm } from "react-hook-form";
+import { useForm, useWatch } from "react-hook-form";
 import { useState } from "react";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
@@ -9,7 +9,6 @@ import { loginUser } from "../../lib/api/auth";
 import { useAuthStore } from "../../lib/store/authStore";
 import { useNavigate } from "react-router-dom";
 import { Icon } from "../Icon/Icon";
-
 
 type LoginFormValues = {
   email: string;
@@ -30,7 +29,7 @@ const schema = yup.object({
 
 export const LoginForm = () => {
   const [error, setError] = useState("");
-  const [value, setValue] = useState("");
+  // const [value, setValue] = useState("");
   const setUser = useAuthStore((state) => state.setUser);
   const navigate = useNavigate();
 
@@ -38,8 +37,11 @@ export const LoginForm = () => {
     register,
     handleSubmit,
     reset,
-    formState: { errors },
+    control,
+    setValue,
+    formState: { errors, dirtyFields },
   } = useForm({
+    mode: "onChange",
     resolver: yupResolver(schema),
   });
 
@@ -60,34 +62,73 @@ export const LoginForm = () => {
     console.log("user logged in");
   };
 
-      const handleClear = (e) => {
-        setValue('');
-        // onSearch('');
-        e.target.value = '';
-    }
+  const email = useWatch({
+    control,
+    name: "email",
+  });
+
+   const password = useWatch({
+    control,
+    name: "password",
+  })
 
   return (
     <form className={css.loginForm} onSubmit={handleSubmit(onSubmit)}>
       <div className={css.formFields}>
         <div className={css.emailPassword}>
+          <input
+            type="text"
+            placeholder="Email"
+            className={` ${css.field} ${errors.email && css.errorField} ${dirtyFields.email && email && !errors.email && css.check} `}
+            {...register("email")}
+          />
+          {errors.email ? (
+            <button
+              type="button"
+              className={css.clearButton}
+              onClick={() => setValue("email", "")}
+            >
+              <Icon name="icon-x" className={css.icon} />
+            </button>
+          ) : (
+            dirtyFields.email &&
+            email &&
+            !errors.email && (
+              <Icon name="icon-check" className={css.iconCheck} />
+            )
+          )}
 
-      <input type="text" placeholder="Email" className={errors.email ? css.errorField : css.field} {...register("email")} />
-      {errors.email && <button type="button" className={css.clearButton} onClick={handleClear}>
-         <Icon name="icon-x" className={css.icon}/></button>}
-      
-      {errors.email && <p className={css.error}>{errors.email.message}</p>}
-      </div>
+          {errors.email && <p className={css.error}>{errors.email.message}</p>}
+        </div>
 
-      <div className={css.emailPassword}>
-
-      <input type="text" placeholder="Password" className={errors.password ? css.errorField : css.field} {...register("password")} />
-      {errors.password && <p>{errors.password.message}</p>}
-      </div>
+        <div className={css.emailPassword}>
+          <input
+            type="password"
+            placeholder="Password"
+            className={`${css.field} ${errors.password && css.errorField} ${dirtyFields.password && password && !errors.email && !errors.password && css.check}`}
+            {...register("password")}
+          />
+          <Icon name="icon-eye-off" className={css.iconCheck} />
+          {errors.password ? (
+            <p className={css.error}>{errors.password.message}</p>
+          ) : (
+            dirtyFields.password &&
+            !errors.password &&
+            password && (
+              <>
+                <p className={css.passwordCorrect}>Password is secure</p>{" "}
+                <Icon name="icon-check" className={css.iconPasswordCheck} />
+              </>
+            )
+          )}
+        </div>
       </div>
 
       {error && <p className={css.error}>{error}</p>}
 
-      <Button type="submit" className={css.button}>LOG IN</Button>
+      <Button type="submit" className={css.button}>
+        LOG IN
+      </Button>
     </form>
   );
 };
