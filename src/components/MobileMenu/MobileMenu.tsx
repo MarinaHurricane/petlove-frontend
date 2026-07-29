75d0b5e4c55d5f69ae73e1bb947a00575570c1bc
MarinaHurricane <maryishch88@gmail.com>
2026-07-30 00:27:37 +0100

    mobile menu styles

--- src/components/MobileMenu/MobileMenu.tsx
+++ src/components/MobileMenu/MobileMenu.tsx
@@ -3,6 +3,9 @@ import { NavLink } from "react-router-dom";
 import css from "./MobileMenu.module.css";
 import { Button } from "../Button/Button";
 import { useAuthStore } from "../../lib/store/authStore";
+import { useState } from "react";
+import { Modal } from "../Modal/Modal";
+import { ModalApproveAction } from "../ModalApproveAction/ModalApproveAction";
 
 type MobileMenuProps = {
   variant: "default" | "home";
@@ -14,35 +17,49 @@ export const MobileMenu = ({
   variant = "default",
 }: MobileMenuProps) => {
   const { user } = useAuthStore();
+  const [isLogoutModalOpen, setIsLogoutModalOpen] = useState(false);
+  const closeIsLogoutModalOpen = () => setIsLogoutModalOpen(false);
   return (
     <>
       {/* <div className={css.backdrop}> */}
       <div className={variant === "default" ? css.menu : css.menuHome}>
         <div className={css.iconWrap} onClick={onClose}>
-          <Icon name="icon-cross-small" className={variant === "default" ? css.icon : css.iconHome}/>
+          <Icon
+            name="icon-cross-small"
+            className={variant === "default" ? css.icon : css.iconHome}
+          />
         </div>
+
+        {/* <div className={css.navWrapper}> */}
 
         <nav>
           <ul className={css.navList}>
             <li className={css.link}>
               {" "}
-              <NavLink to="/news"  >
+              <NavLink to="/news">
                 {" "}
-                <Button className={css.button} onClick={onClose}>News</Button>
+                <Button className={css.button} onClick={onClose}>
+                  News
+                </Button>
               </NavLink>
             </li>
             <li>
               {" "}
-              <NavLink to="/pets" >
+              <NavLink to="/pets">
                 {" "}
-                <Button className={css.button} onClick={onClose}>Find a pet</Button>
+                <Button className={css.button} onClick={onClose}>
+                  Find a pet
+                </Button>
               </NavLink>
             </li>
             <li>
               {" "}
-              <NavLink to="/friends" >
+              <NavLink to="/friends">
                 {" "}
-                <Button className={css.button} onClick={onClose}> Our friends</Button>
+                <Button className={css.button} onClick={onClose}>
+                  {" "}
+                  Our friends
+                </Button>
               </NavLink>
             </li>
           </ul>
@@ -50,26 +67,44 @@ export const MobileMenu = ({
         </nav>
 
         {user ? (
-          <Button>LOG OUT</Button>
+          <Button
+            className={css.authList}
+            variant="secondary"
+            onClick={() => setIsLogoutModalOpen(true)}
+          >
+            LOG OUT
+          </Button>
         ) : (
           <nav>
-            <ul className={css.navList}>
-              <li>
+            <ul className={css.authList}>
+              <li >
                 {" "}
-                <NavLink to="/login" >
-                  <Button className={css.button} onClick={onClose}>LOG IN</Button>
+                <NavLink to="/login">
+                  <Button className={css.button} onClick={onClose}>
+                    LOG IN
+                  </Button>
                 </NavLink>
               </li>
-              <li>
+              <li >
                 {" "}
-                <NavLink to="/register" >
-                  <Button className={css.button} onClick={onClose}>REGISTRATION</Button>
+                <NavLink to="/register">
+                  <Button className={css.button} onClick={onClose}>
+                    REGISTRATION
+                  </Button>
                 </NavLink>
               </li>
             </ul>
           </nav>
         )}
-      </div>
+        </div>
+         {/* </div> */}
+
+        {isLogoutModalOpen && (
+          <Modal onClose={closeIsLogoutModalOpen}>
+            <ModalApproveAction onClose={closeIsLogoutModalOpen} />
+          </Modal>
+        )}
+     
       {/* </div> */}
     </>
   );
